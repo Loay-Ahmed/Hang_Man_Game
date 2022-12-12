1172fd61284c459d22b85ce31a87ee62ab2f1c37
Loay-Ahmed <loayabelgendy@gmail.com>
2022-12-12 21:31:59 +0200

    Added main() function

--- hang_man_game.py
+++ hang_man_game.py
@@ -1,13 +1,85 @@
-hang_man = ["""
-  _______
- |       |
- |       O
- |      /|\\
- |      / \\
-_|_
-""",
-               " "
-]
-for i in hang_man:
-    print(i)
-    
+"""Hang Man Game"""
+
+import random
+
+with open("words.txt", "r") as file:
+    words = (file.read()).split(" ")
+
+print(words)
+# different stages for wrong guess
+hangman = ["""
+  ______
+ |
+ |
+ |
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|\\
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|\\
+ |     /
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|\\
+ |     / \\
+_|__"""
+           ]
+
+# random choice from the words[]
+word = random.choice(words)
+txt = ['_'] * len(word)
+indexes = []
+i = 0
+
+while i <= 5:
+    print(" ".join(txt))
+    letter = input("Enter a letter: ")
+
+    while letter.isalpha() is False:
+        letter = input("Enter a letter: ")
+
+    if letter in word:
+        indexes.clear()
+        for (index, char) in enumerate(word):
+            if char == letter:
+                indexes.append(index)
+
+        for index in indexes:
+            txt[index] = letter
+    else:
+        i += 1
+        for j in range(0, i):
+            print(hangman[j])
+
+    if word == "".join(txt):
+        print("\n==>You WIN")
+        break
+
+if i > 5:
+    print(f"Better Luck next time - the secret word is: {word}")
