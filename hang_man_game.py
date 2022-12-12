386df7f62105ceab5629c4b8b02f9d603d140c79
Loay-Ahmed <loayabelgendy@gmail.com>
2022-12-12 21:31:59 +0200

    Added main() function

--- hang_man_game.py
+++ hang_man_game.py
@@ -1,13 +1,67 @@
-hang_man = ["""
-  _______
- |       |
- |       O
- |      /|\\
- |      / \\
-_|_
-""",
-               " "
-]
-for i in hang_man:
-    print(i)
-    
+"""Hang Man Game"""
+
+import random
+
+words = ["ant", "cat", "woman", "man", "dog", "snake", "elephant", "giraffe", "ostrich",
+         "monkey", "rhino", "hippo", "chipmunk"]
+
+# diferent stages for wrong guess
+hangman = ["""
+  ______
+ |
+ |
+ |
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|\\
+ |
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|\\
+ |     /
+_|__""",
+           """
+  ______
+ |      |
+ |      O
+ |     /|\\
+ |     / \\
+_|__"""
+           ]
+
+# random choice from the words[]
+word = random.choice(words)
+
+
+def text(wrd):
+    """Hiding the characters of the word"""
+
+    txt = "_ "
+    txt *= len(wrd)
+    return txt
+
+
+def main():
+    print(text(word))
+    """ The main function that combines all the other functions """
